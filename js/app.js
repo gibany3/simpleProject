eda85059c3e2053ffc942146b8f0ee9b81297abc
mjarosz <mjarosz@pgs-soft.com>
2016-09-01 16:21:44 +0200

    Add object literal

--- js/app.js
+++ js/app.js
@@ -37,4 +37,50 @@ function debounce(func, wait, immediate) {
 }
 
 
+// var myFeature = {
+//     config : {
+//         wrapper : '#myFeature',
+//         container : 'div',
+//         urlBase : 'foo.php?item='
+//     },
+//
+//     init : function(config) {
+//         $.extend(myFeature.config, config);
+//         $(myFeature.config.wrapper).find('li').
+//         each(function() {
+//             myFeature.getContent($(this));
+//         }).
+//         click(function() {
+//             myFeature.showContent($(this));
+//         });
+//     },
+//
+//     setHeightOnLoad : function() {
+//
+//         var arrowSpan = $(".arrow-img");
+//         var imgHeight = $(".first-img-block").find("img").eq(0).height();
+//         var halfSize = $(((imgHeight) / 2) - 15)[0];
+//
+//         arrowSpan.height(imgHeight);
+//         arrowSpan.find("span").css("top", halfSize);
+//     },
+//
+//     getContent : function($li) {
+//         $li.append(myFeature.config.container);
+//         var url = myFeature.buildUrl($li);
+//         $li.find(myFeature.config.container).load(url);
+//     },
+//
+//     showContent : function($li) {
+//         $li.find('div').show();
+//         myFeature.hideContent($li.siblings());
+//     },
+//
+//     hideContent : function($elements) {
+//         $elements.find('div').hide();
+//     }
+// };
+//
+// $(document).ready(function() { myFeature.init(); });
+
 
