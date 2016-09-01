d755e3362dc528375214e8601e4280867a853d56
mjarosz <mjarosz@pgs-soft.com>
2016-09-01 12:55:38 +0200

    Change resize function

--- js/app.js
+++ js/app.js
@@ -1,38 +1,40 @@
-$(document).ready(function() {
-
-    var arrowSpan = $(".arrow-img").find("span");
-    var imgHeight = $(".first-img-block").find("img").eq(0).height();
-    var halfSize = $(((imgHeight)/2)-15)[0];
-
-    $(".arrow-img").height(imgHeight);
-    arrowSpan.css("top", halfSize);
-
-    var resizeFunction = debounce(function() {
-        // All the taxing stuff you do
-        var imgHeight = $(".first-img-block").find("img").eq(0).height();
-        var halfSize = $(((imgHeight)/2)-15)[0];
-        
-        $(".arrow-img").height(imgHeight);
-
-        arrowSpan.css("top", halfSize);
-    }, 250);
-
-    function debounce(func, wait, immediate) {
-        var timeout;
-        return function() {
-            var context = this, args = arguments;
-            var later = function() {
-                timeout = null;
-                if (!immediate) func.apply(context, args);
-            };
-            var callNow = immediate && !timeout;
-            clearTimeout(timeout);
-            timeout = setTimeout(later, wait);
-            if (callNow) func.apply(context, args);
-        };
-    };
-
-
+$(document).ready(function () {
+    setHeightOnLoad();
     window.addEventListener('resize', resizeFunction);
-
 });
+
+
+function setHeightOnLoad() {
+
+    var arrowSpan = $(".arrow-img");
+    var imgHeight = $(".first-img-block").find("img").eq(0).height();
+    var halfSize = $(((imgHeight) / 2) - 15)[0];
+
+    arrowSpan.height(imgHeight);
+    arrowSpan.find("span").css("top", halfSize);
+}
+
+
+var resizeFunction = debounce(function () {
+    // All the taxing stuff you do
+    setHeightOnLoad();
+}, 250);
+
+
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
+
+
