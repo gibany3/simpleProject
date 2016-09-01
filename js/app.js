6cc90d6ed1f2a6f40b2d833f2db5e4b0a3a7431b
Michal Jarosz (PGS Software) <mjarosz@pgs-soft.com>
2016-09-01 08:29:44 +0200

    add function debounced jquery

--- js/app.js
+++ js/app.js
@@ -7,40 +7,32 @@ $(document).ready(function() {
     $(".arrow-img").height(imgHeight);
     arrowSpan.css("top", halfSize);
 
-
-    $( window ).resize(function() {
+    var resizeFunction = debounce(function() {
+        // All the taxing stuff you do
         var imgHeight = $(".first-img-block").find("img").eq(0).height();
         var halfSize = $(((imgHeight)/2)-15)[0];
-
-
+        
         $(".arrow-img").height(imgHeight);
 
         arrowSpan.css("top", halfSize);
-    });
+    }, 250);
+
+    function debounce(func, wait, immediate) {
+        var timeout;
+        return function() {
+            var context = this, args = arguments;
+            var later = function() {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    };
 
 
-
-    // var myEfficientFn = debounce(function() {
-    //     // All the taxing stuff you do
-    // }, 250);
-    //
-    // function debounce(func, wait, immediate) {
-    //     var timeout;
-    //     return function() {
-    //         var context = this, args = arguments;
-    //         var later = function() {
-    //             timeout = null;
-    //             if (!immediate) func.apply(context, args);
-    //         };
-    //         var callNow = immediate && !timeout;
-    //         clearTimeout(timeout);
-    //         timeout = setTimeout(later, wait);
-    //         if (callNow) func.apply(context, args);
-    //     };
-    // };
-    //
-    // window.addEventListener('resize', myEfficientFn);
-
-
+    window.addEventListener('resize', resizeFunction);
 
 });
